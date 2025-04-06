f2a0dfe6b18150fbfa53eaf952e33441803ea464
lijoe peter <lijoepeter.15@gmail.com>
2025-04-06 19:10:08 +0900

    second

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,51 @@
+use std::string;
+
 
 pub const  SIZE:usize = 6;
 fn main() {
    let  u_matrix = matrix_creation();
    let  r_matrix = u_matrix;
-   
-   get_3x3(&u_matrix,1,1);
-  println!("result");
+   let mut state :bool = get_3x3(&u_matrix,1,1);
+   if state{
+      println!("the cell is alive");
+   }else{
+      println!("the cell is dead");
+   }
+  
   for row in &r_matrix {
    println!("{:?}", row);
    }
 }
-fn get_3x3(mat:&[[i8;6];6],row:usize,col:usize){
-   let mut sub_mat=[[0;3];3];
+fn get_3x3(mat:&[[i8;6];6],row:usize,col:usize)-> bool{
+   let mut  count:i8 =0;
+   let mut alive=true;
    for i in 0..3{
       for j in 0..3{
-         sub_mat[i][j] = mat[i+row][j+col];
+         if i == 1 && j== 1 && mat[i+row][j+col] == 1{
+            alive = true;//true if the state is alive
+            println!("the cell was alive");
+         }else if  i == 1 && j== 1 && mat[i+row][j+col] == 0{
+            alive = false;//false if the state is dead
+            println!("the cell was dead");
+         }
+         else{
+            count = count + mat[i+row][j+col];
+         }
       }
    }
-   for row in &sub_mat {
-      println!("{:?}", row);
-      }
+   println!("the cell is has {count} neighbour");
+   if !alive && count>2{
+      alive =true
+   }
+   if alive && (count==0 || count>3) {
+      alive = false; //overpopulation&isolated
+   }
+   alive
 }
 
 fn matrix_creation()->[[i8;SIZE];SIZE]{
    const RANGE:i32 = SIZE as i32;
-   let arr1:[i8;SIZE]=[0,0,0,0,0,0];
+   let arr1:[i8;SIZE]=[0,1,0,0,0,0];
    let  mut matrix:[[i8;SIZE];SIZE]=[[0; SIZE]; SIZE];
    let mut i:i32 = 0;
    loop {
