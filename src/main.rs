27e7d8592ec2b184673bca03f71d5f633f5c040e
lijoe peter <lijoepeter.15@gmail.com>
2025-04-09 01:31:15 +0900

    manual but works

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,103 @@
-use std::string;
+use std::thread;
+use std::time::Duration;
 
-
-pub const  SIZE:usize = 6;
+pub const  SIZE:usize = 18;
 fn main() {
-   let  u_matrix = matrix_creation();
-   let  r_matrix = u_matrix;
-   let mut state :bool = get_3x3(&u_matrix,0,2);
-   //hange this hard coded num to fetch from (+1+1 to -last-last mid point)butloop will be for 0,0 to last-3,last-3
-   if state{
-      println!("the cell is alive");
-   }else{
-      println!("the cell is dead");
+   let  mut u_matrix = matrix_creation();
+   let  mut r_matrix = u_matrix;
+   print!("\x1B[2J\x1B[1;1H");//clearing the screen
+   for row in &u_matrix {
+      println!("{:?}", row);
+      }
+   loop{
+      set_result(&u_matrix,&mut r_matrix,SIZE);
+      println!("");
+
+      thread::sleep(Duration::from_secs(1));//waiting for 1 second 
+      print!("\x1B[2J\x1B[1;1H");//clearing the screen
+
+
+      for row in &r_matrix {
+         println!("{:?}", row);
+         }
+      if u_matrix == r_matrix{
+         break;
+      }
+      u_matrix = r_matrix;
    }
-  
-  for row in &r_matrix {
-   println!("{:?}", row);
+
+}
+fn set_result(int_mat:&[[i8;SIZE];SIZE],r_matrix:&mut [[i8;SIZE];SIZE],size:usize){
+   for pos_x in 0..size-2{
+      for pos_y in 0..size-2{
+         // let result =if get_3x3(int_mat,pos_x,pos_y) {1} else {0};
+         // println!("{}",result);
+         // r_matrix[pos_x+1][pos_y+1] = result;
+         r_matrix[pos_x+1][pos_y+1] = if get_3x3(int_mat,pos_x,pos_y) {1} else {0};
+         // println!("to position {},{}",pos_x+1,pos_y+1);
+      }
    }
 }
 
-fn get_3x3(mat:&[[i8;6];6],row:usize,col:usize)-> bool{
+fn get_3x3(mat:&[[i8;SIZE];SIZE],row:usize,col:usize)-> bool{
    let mut  count:i8 =0;
    let mut alive=true;
    for i in 0..3{
       for j in 0..3{
          if i == 1 && j== 1 && mat[i+row][j+col] == 1{
             alive = true;//true if the state is alive
-            println!("the cell was alive");
+            // println!("the cell was alive");
          }else if  i == 1 && j== 1 && mat[i+row][j+col] == 0{
             alive = false;//false if the state is dead
-            println!("the cell was dead");
+            // println!("the cell was dead");
          }
          else{
             count = count + mat[i+row][j+col];
          }
       }
    }
-   println!("the cell is has {count} neighbour");
+   // println!("the cell has {count} neighbour");
    if !alive && count>2{
       alive =true//birth
    }
-   if alive && (count==0 || count>3) {
+   if alive && (count<2 || count>3) {
       alive = false; //overpopulation&isolated
    }
    alive
 }
 
 fn matrix_creation()->[[i8;SIZE];SIZE]{
-   const RANGE:i32 = SIZE as i32;
-   let arr1:[i8;SIZE]=[0,1,0,0,0,0];
-   let  mut matrix:[[i8;SIZE];SIZE]=[[0; SIZE]; SIZE];
-   let mut i:i32 = 0;
-   loop {
-      matrix[i as usize]=arr1;
-      if i >=RANGE-1{
-         break;
-      }
-      i = i+1;
-   }
-  matrix
+   let matrix:[[i8;SIZE];SIZE]=[ [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
+     matrix
 }
+// fn matrix_creation()->[[i8;SIZE];SIZE]{
+//    const RANGE:i32 = SIZE as i32;
+//    let arr1:[i8;SIZE]=[0,1,0,0,0,0];
+//    let  mut matrix:[[i8;SIZE];SIZE]=[[0; SIZE]; SIZE];
+//    let mut i:i32 = 0;
+//    loop {
+//       matrix[i as usize]=arr1;
+//       if i >=RANGE-1{
+//          break;
+//       }
+//       i = i+1;
+//    }
+//   matrix
+// }
